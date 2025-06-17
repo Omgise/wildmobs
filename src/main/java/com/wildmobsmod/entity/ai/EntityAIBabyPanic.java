e2211372a0194bc487e1ebe9b0af7f09609f6dbe
JohnButlerThird <112212674+JohnButlerThird@users.noreply.github.com>
2025-06-17 15:37:15 -0400

    fixes aibabypanic crash

--- src/main/java/com/wildmobsmod/entity/ai/EntityAIBabyPanic.java
+++ src/main/java/com/wildmobsmod/entity/ai/EntityAIBabyPanic.java
@@ -1,14 +1,8 @@
 package com.wildmobsmod.entity.ai;
 
-import java.util.Iterator;
-import java.util.List;
-
 import net.minecraft.entity.EntityAgeable;
-import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.RandomPositionGenerator;
-import net.minecraft.entity.passive.EntityAnimal;
-import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.Vec3;
 
 public class EntityAIBabyPanic extends EntityAIBase {
@@ -37,34 +31,6 @@ public class EntityAIBabyPanic extends EntityAIBase {
         this.randPosY = vec3.yCoord;
         this.randPosZ = vec3.zCoord;
         return true;
-    }
-
-    /**
-     * Execute a one shot task or start executing a continuous task
-     */
-    public void startExecuting() {
-        this.creature.getNavigator()
-            .tryMoveToXYZ(this.randPosX, this.randPosY, this.randPosZ, this.speed);
-        AxisAlignedBB aabb = AxisAlignedBB
-            .getBoundingBox(
-                this.creature.posX,
-                this.creature.posY,
-                this.creature.posZ,
-                this.creature.posX + 1.0D,
-                this.creature.posY + 1.0D,
-                this.creature.posZ + 1.0D)
-            .expand(8.0D, 8.0D, 8.0D);
-        List list = this.creature.worldObj.getEntitiesWithinAABB(this.creature.getClass(), aabb);
-        Iterator iterator = list.iterator();
-        while (iterator.hasNext()) {
-            EntityCreature entitycreature = (EntityCreature) iterator.next();
-            if (this.creature != entitycreature && entitycreature.getAttackTarget() == null
-                && ((EntityAnimal) entitycreature).getAge() >= 0
-                && !entitycreature.isOnSameTeam(this.creature.getAITarget())
-                && this.creature.worldObj.rand.nextInt(2) == 0) {
-                entitycreature.setAttackTarget(this.creature.getAITarget());
-            }
-        }
     }
 
     /**
